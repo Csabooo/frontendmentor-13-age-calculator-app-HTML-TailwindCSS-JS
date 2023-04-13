b9338bfc0507cdb146d44716f8c3ce2855cd9693
Müller Csaba <csaba.muller@hotmail.com>
2023-04-13 21:46:56 +0200

    tailwindcss installed, color, fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /*       ### Primary */
+        "purple": "hsl(259, 100%, 65%)",
+        "light-red": "hsl(0, 100%, 67%)",
+  
+        /*       ### Neutral */
+        "white": "hsl(0, 0%, 100%)",
+        "off-white": "hsl(0, 0%, 94%)",
+        "light-grey": "hsl(0, 0%, 86%)",
+        "smokey-grey": "hsl(0, 1%, 44%)",
+        "off-black": "hsl(0, 0%, 8%)"
+      },
+      fontFamily:{
+        serif: ["Poppins", "san-serif"],
+      },
+    },
+
   },
   plugins: [],
 }
