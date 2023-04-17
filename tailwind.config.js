c3058ecee0b50dbada777dadb5c39d3266e6c524
Müller Csaba <csaba.muller@hotmail.com>
2023-04-17 23:26:37 +0200

    mobile design completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         /*       ### Primary */
         "purple": "hsl(259, 100%, 65%)",
         "light-red": "hsl(0, 100%, 67%)",
-  
+
         /*       ### Neutral */
         "white": "hsl(0, 0%, 100%)",
         "off-white": "hsl(0, 0%, 94%)",
@@ -15,11 +15,35 @@ module.exports = {
         "smokey-grey": "hsl(0, 1%, 44%)",
         "off-black": "hsl(0, 0%, 8%)"
       },
-      fontFamily:{
+      fontFamily: {
         serif: ["Poppins", "san-serif"],
       },
-    },
+      fontSize: {
+        "32": "2rem",
+      },
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+        widest: '.1em',
+        widest: '.3em',
+      },
+      borderRadius: {
+        'none': '0',
+        'sm': '0.125rem',
+        DEFAULT: '0.25rem',
+        DEFAULT: '4px',
+        'md': '0.375rem',
+        'lg': '0.5rem',
+        'full': '9999px',
+        'large': '12px',
+        "xlarge": "92px",
+      },
 
-  },
-  plugins: [],
+    },
+    plugins: [],
+  }
 }
